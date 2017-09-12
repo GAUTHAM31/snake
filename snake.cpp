8ae26f72d0692df9909169aaddbbb84ff6818b5f
gautham31 <gauthamgajith@yahoo.co.in>
2017-09-12 14:53:07 -0400

    creating window

--- snake.cpp
+++ snake.cpp
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 //gl lib
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
+#include <GL/glut.h>
 
 //main function
-int main()
+int main(int argc, char** argv)
 {
 	/* code */
-	if( !glfwInit() )
-{
- exit(EXIT_FAILURE);
-}
-
+	glutInit(&argc, argv);// take -lgl ,lglu ,lglut from commend
+	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB); // use single render frame,init rcb buffer
+	glutInitWindowSize(600, 600);//height,width
+	glutInitWindowPosition(0,0 );
+	glutCreateWindow("Snake");  //to create window with title
+   // glutDisplayFunc(display);  
+    glutMainLoop();  //repeat main loop infinity
 	return 0;
 }
