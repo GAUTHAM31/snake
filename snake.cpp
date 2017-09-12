b213e0125d9cdbb91d676638ded8ba51771d6d49
gautham31 <gauthamgajith@yahoo.co.in>
2017-09-12 09:47:36 -0400

    init and backup

---
+++ snake.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+//gl lib
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+
+//main function
+int main()
+{
+	/* code */
+	if( !glfwInit() )
+{
+ exit(EXIT_FAILURE);
+}
+
+	return 0;
+}
