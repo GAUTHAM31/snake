601017af247ab2798fe2569b956d0c17d275ecb5
gautham31 <gauthamgajith@yahoo.co.in>
2017-09-13 11:52:14 -0400

    final snake with food

--- snake.cpp
+++ snake.cpp
@@ -18,9 +18,50 @@
  bool food_available = false;
  int food_coords[2];
  int move_speed = 100;
- int growth_stage = 0;
-int growth = 2;
+ int growth_stage = 0;//
+int growth = 2;//
 //display function
+void food(){//to create food 
+    if(!food_available){
+        while(true){
+            bool collides = false;
+
+            // Produce a temporary random coordinate
+            int temp_food_coords[2] = { food_coords[0] = 2 * (rand() % ((int) map_half_length + 1)) - (int) map_half_length,
+                                        food_coords[1] = 2 * (rand() % ((int) map_half_length + 1)) - (int) map_half_length };
+
+            // Does it collide with the snake?
+            for(unsigned int a = 0; a < part_coords.size(); a++){
+                if(temp_food_coords[0] == part_coords[a][0] &&
+                   temp_food_coords[1] == part_coords[a][1]){
+                    collides = true;
+                }
+            }
+
+            // If it doesn't collide with the snake, then make it the real food coordinates
+            if(collides == false){
+                food_coords[0] = temp_food_coords[0];
+                food_coords[1] = temp_food_coords[1];
+
+                food_available = true;
+
+                break;
+            }
+        }
+    }
+
+    glLoadIdentity();
+    glTranslatef(food_coords[0], food_coords[1], -40.0f);
+    glColor3f(1.0f, 0.0f, 0.0f);
+
+    glBegin(GL_POLYGON);
+        glVertex2d( 1.0f,  1.0f);
+        glVertex2d( 1.0f, -1.0f);
+        glVertex2d(-1.0f, -1.0f);
+        glVertex2d(-1.0f,  1.0f);
+    glEnd();
+}
+
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -33,7 +74,7 @@ void display()
     for(unsigned int a = 0; a < part_coords.size(); a++){
         glLoadIdentity();
         glTranslatef(part_coords[a][0], part_coords[a][1], -40.0f);
-        glColor3f(0.0f, 1.0f, 0.0f);
+        glColor3f(1.0f, 1.0f, 1.0f);
 
         glBegin(GL_POLYGON);
             glVertex2d( 1.0f,  1.0f);
@@ -44,7 +85,7 @@ void display()
     }
 
 
-
+    food();
     glutSwapBuffers();
 }
 void moveSnake(int new_direction)
@@ -52,10 +93,10 @@ void moveSnake(int new_direction)
 direction = new_direction;
 
     int last_part = part_coords.size() - 1;
-    std::deque<float> new_head = part_coords[last_part];
+    std::deque<float> new_head = part_coords[last_part];//to find head (-_-)
 
     if(direction == UP){
-        // Did we slither into ourself?
+        // Did we hit ourself?
         for(unsigned int a = 0; a < part_coords.size(); a++){
             if(part_coords[0][0]        == part_coords[a][0] &&
                part_coords[0][1] + 2.0f == part_coords[a][1]){
@@ -63,7 +104,7 @@ direction = new_direction;
             }
         }
 
-        // Did we slither into a wall?
+        // Did we hit a wall?
         if(part_coords[0][1] == map_half_length){
             exit(0);
         }
@@ -75,9 +116,9 @@ direction = new_direction;
             food_available = false;
         }
 
-        new_head[1] = part_coords[0][1] + 2.0f;
+        new_head[1] = part_coords[0][1] + 2.0f;//moving
     } else if(direction == DOWN){
-        // Did we slither into ourself?
+        // Did we hit ourself?
         for(unsigned int a = 0; a < part_coords.size(); a++){
             if(part_coords[0][0]        == part_coords[a][0] &&
                part_coords[0][1] - 2.0f == part_coords[a][1]){
@@ -85,7 +126,7 @@ direction = new_direction;
             }
         }
 
-        // Did we slither into a wall?
+        // Did we hit a wall?
         if(part_coords[0][1] == -map_half_length){
             exit(0);
         }
@@ -103,7 +144,7 @@ direction = new_direction;
     }
 
     if(direction == LEFT){
-        // Did we slither into ourself?
+        // Did we hit ourself?
         for(unsigned int a = 0; a < part_coords.size(); a++){
             if(part_coords[0][0] - 2.0f == part_coords[a][0] &&
                part_coords[0][1]        == part_coords[a][1]){
@@ -111,7 +152,7 @@ direction = new_direction;
             }
         }
 
-        // Did we slither into a wall?
+        // Did we hit a wall?
         if(part_coords[0][0] == -map_half_length){
             exit(0);
         }
@@ -125,7 +166,7 @@ direction = new_direction;
 
         new_head[0] = part_coords[0][0] - 2.0f;
     } else if(direction == RIGHT){
-        // Did we slither into ourself?
+        // Did we hit ourself?
         for(unsigned int a = 0; a < part_coords.size(); a++){
             if(part_coords[0][0] + 2.0f == part_coords[a][0] &&
                part_coords[0][1]        == part_coords[a][1]){
@@ -133,7 +174,7 @@ direction = new_direction;
             }
         }
 
-        // Did we slither into a wall?
+        // Did we hit a wall?
         if(part_coords[0][0] == map_half_length){
             exit(0);
         }
@@ -150,10 +191,10 @@ direction = new_direction;
         new_head[0] = part_coords[0][0];
     }
 
-    part_coords.push_front(new_head);
+    part_coords.push_front(new_head);//adds new head
 
-    if(!growth_stage){
-        part_coords.pop_back();
+   if(!growth_stage){
+        part_coords.pop_back();//pop head if it is no growth
     } else if(growth_stage == growth){
         growth_stage = 0;
     } else {
@@ -235,14 +276,15 @@ int main(int argc, char** argv)
 	/* code */
 	int windowWidth=600;
 	int windowHeight=600;
+	//to make snake at first 
 	int initSize = 3;
 
     // Specify the coordinates to each part of the snake
     for(int a = 1; a <= initSize; a++){
         std::deque<float> row;
 
-        row.push_back(0.0f);
-        row.push_back((map_half_length + 2.0f + (initSize * 2)) - (a * 2));
+        row.push_back(0.0f);//x
+        row.push_back((map_half_length + 2.0f + (initSize * 2)) - (a * 2));//y
 
         part_coords.push_front(row);
     }
