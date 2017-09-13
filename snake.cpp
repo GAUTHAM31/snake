2075e5da8f7400e864864509ed42fef96cfc8425
gautham31 <gauthamgajith@yahoo.co.in>
2017-09-13 01:24:55 -0400

    moving snake

--- snake.cpp
+++ snake.cpp
@@ -2,11 +2,24 @@
 #include <stdlib.h>
 #include <iostream>
 //gl lib
-#include <deque>
+#include <deque> //for data structure
 #include <GL/glut.h>
+
+//direction
+#define UP    1
+#define DOWN  2
+#define LEFT  3
+#define RIGHT 4
 //data structure to represnt snake
  std::deque< std::deque<float> > part_coords;
  float map_half_length = 30.0f;
+ int direction = DOWN;
+ bool moved = false;
+ bool food_available = false;
+ int food_coords[2];
+ int move_speed = 100;
+ int growth_stage = 0;
+int growth = 2;
 //display function
 void display()
 {
@@ -34,10 +47,124 @@ void display()
 
     glutSwapBuffers();
 }
+void moveSnake(int new_direction)
+{
+direction = new_direction;
+
+    int last_part = part_coords.size() - 1;
+    std::deque<float> new_head = part_coords[last_part];
+
+    if(direction == UP){
+        // Did we slither into ourself?
+        for(unsigned int a = 0; a < part_coords.size(); a++){
+            if(part_coords[0][0]        == part_coords[a][0] &&
+               part_coords[0][1] + 2.0f == part_coords[a][1]){
+                exit(0);
+            }
+        }
+
+        // Did we slither into a wall?
+        if(part_coords[0][1] == map_half_length){
+            exit(0);
+        }
+
+        // Did we get food?
+        if(part_coords[0][0]        == food_coords[0] &&
+           part_coords[0][1] + 2.0f == food_coords[1]){
+            growth_stage++;
+            food_available = false;
+        }
+
+        new_head[1] = part_coords[0][1] + 2.0f;
+    } else if(direction == DOWN){
+        // Did we slither into ourself?
+        for(unsigned int a = 0; a < part_coords.size(); a++){
+            if(part_coords[0][0]        == part_coords[a][0] &&
+               part_coords[0][1] - 2.0f == part_coords[a][1]){
+                exit(0);
+            }
+        }
+
+        // Did we slither into a wall?
+        if(part_coords[0][1] == -map_half_length){
+            exit(0);
+        }
+
+        // Did we get food?
+        if(part_coords[0][0]        == food_coords[0] &&
+           part_coords[0][1] - 2.0f == food_coords[1]){
+            growth_stage++;
+            food_available = false;
+        }
+
+        new_head[1] = part_coords[0][1] - 2.0f;
+    } else {
+        new_head[1] = part_coords[0][1];
+    }
+
+    if(direction == LEFT){
+        // Did we slither into ourself?
+        for(unsigned int a = 0; a < part_coords.size(); a++){
+            if(part_coords[0][0] - 2.0f == part_coords[a][0] &&
+               part_coords[0][1]        == part_coords[a][1]){
+                exit(0);
+            }
+        }
+
+        // Did we slither into a wall?
+        if(part_coords[0][0] == -map_half_length){
+            exit(0);
+        }
+
+        // Did we get food?
+        if(part_coords[0][0] - 2.0f == food_coords[0] &&
+           part_coords[0][1]        == food_coords[1]){
+            growth_stage++;
+            food_available = false;
+        }
+
+        new_head[0] = part_coords[0][0] - 2.0f;
+    } else if(direction == RIGHT){
+        // Did we slither into ourself?
+        for(unsigned int a = 0; a < part_coords.size(); a++){
+            if(part_coords[0][0] + 2.0f == part_coords[a][0] &&
+               part_coords[0][1]        == part_coords[a][1]){
+                exit(0);
+            }
+        }
+
+        // Did we slither into a wall?
+        if(part_coords[0][0] == map_half_length){
+            exit(0);
+        }
+
+        // Did we get food?
+        if(part_coords[0][0] + 2.0f == food_coords[0] &&
+           part_coords[0][1]        == food_coords[1]){
+            growth_stage++;
+            food_available = false;
+        }
+
+        new_head[0] = part_coords[0][0] + 2.0f;
+    } else {
+        new_head[0] = part_coords[0][0];
+    }
+
+    part_coords.push_front(new_head);
+
+    if(!growth_stage){
+        part_coords.pop_back();
+    } else if(growth_stage == growth){
+        growth_stage = 0;
+    } else {
+        growth_stage++;
+    }
+
+    glutPostRedisplay();
+}
 //keyboard function
 void keyboard(int key, int x, int y){
-    UNUSED(x);
-    UNUSED(y);
+
 
     switch(key){
         case GLUT_KEY_UP:{
@@ -83,7 +210,24 @@ void keyboard(int key, int x, int y){
 
     glutPostRedisplay();
 }
+void moveSnakeAuto(int value){
+    if(!moved){
 
+        if(direction == UP){
+            moveSnake(UP);
+        } else if(direction == DOWN){
+            moveSnake(DOWN);
+        } else if(direction == LEFT){
+            moveSnake(LEFT);
+        } else if(direction == RIGHT){
+            moveSnake(RIGHT);
+        }
+    } else {
+        moved = false;
+
+    }
+     glutTimerFunc(move_speed, moveSnakeAuto, 0);
+}
 
 //main function
 int main(int argc, char** argv)
@@ -102,8 +246,6 @@ int main(int argc, char** argv)
 
         part_coords.push_front(row);
     }
-    for(int i=0;i<3;i++)
-    std::cout<<"-"<<part_coords[i][0]<<" "<<part_coords[i][1];
 
 	///glut functions
 	glutInit(&argc, argv);// take -lgl ,lglu ,lglut from commend
@@ -113,6 +255,7 @@ int main(int argc, char** argv)
 	glutCreateWindow("Snake");  //to create window with title
     glutDisplayFunc(display);  //calls function to draw
     glutSpecialFunc(keyboard);
+    glutTimerFunc(move_speed, moveSnakeAuto, 0);
 
     glViewport( 0, 0, windowWidth, windowHeight );
     glMatrixMode( GL_PROJECTION );//GL_PROJECTION - for camera positioning 
